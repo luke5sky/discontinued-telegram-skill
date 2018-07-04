6e9d8d7a5b49b9fdc72a2b8c71563f338647050f
Luke5Sky <luke.gangel87@gmail.com>
2018-07-04 17:29:30 +0200

    Shutdown and Whitelist added

--- __init__.py
+++ __init__.py
@@ -28,53 +28,66 @@ from telegram.ext import Updater, MessageHandler, Filters, CommandHandler
 from websocket import create_connection, WebSocket
 from mycroft.messagebus.message import Message
 
+
 __author__ = 'luke5sky'
 
 class TelegramSkill(MycroftSkill):
 
     def __init__(self):
         super(TelegramSkill, self).__init__(name="TelegramSkill")
-        
+        witty = 346018160
+        chat_whitelist = [witty,'123']
+#	    get_intro_message()
 ################################################################################
         def telegramMessages(bot, update):
             msg = update.message.text
             chat_id = update.message.chat_id
-            print("Telegram-Message from User: " + msg)
-            uri = 'ws://localhost:8181/core'
-            #ws.close()
-            ws = create_connection(uri)
-            message = '{"context": null, "type": "recognizer_loop:utterance", "data": {"lang": "en-us", "utterances": ["' + msg + '"]}}'
-            result = ws.send(message)
-        #    print("Message sent" + message)
-    
-            mtime = time.time()
-            count = 0
-            while count < 5:
-                  test_resp = ws.recv()
-                  if ('"utterance"' in test_resp) and ('"speak"' in test_resp):
-             #        print("Found response: " + test_resp)
-                     response = re.sub(".*utterance\":\s\"([\w\s'(),.;:?!]+)\".*", r"\1", test_resp)
-                     print("Response to Telegram Message found: " + response)
-                     bot.send_message(chat_id=chat_id, text=response)
-                     mtime = time.time()
-                     break
-                  now = time.time()
-                  count = now - mtime
-            ws.close()
+            
+            if chat_id in chat_whitelist:
+               print("Telegram-Message from User: " + msg)
+               uri = 'ws://localhost:8181/core'
+               ws = create_connection(uri)
+               utt = '{"context": null, "type": "recognizer_loop:utterance", "data": {"lang": "en-us", "utterances": ["' + msg + '"]}}'
+               
+               if not chat_id in chat_whitelist:
+                  print("WHITECHat_ID is: %d" % chat_id)
+               ws.send(utt)
+ #              def handle_listener_started(self):
+ #                  print("listener started wupdidudl: ")
+ #              self.add_event('recognizer_loop:utterance', handle_listener_started)
+               mtime = time.time()
+               count = 0
+               while count < 10:
+                     test_resp = ws.recv()
+                     if ('"utterance"' in test_resp) and ('"speak"' in test_resp):
+                #        print("Found response: " + test_resp)
+                        response = re.sub(".*utterance\":\s\"([\w\s'(),.;:?!]+)\".*", r"\1", test_resp)
+                        print("Response to Telegram Message found: " + response)
+                        bot.send_message(chat_id=chat_id, text=response)
+                        mtime = time.time()
+                        break
+                     now = time.time()
+                     count = now - mtime
+               ws.close()
+            else:
+               print("Chat ID is not whitelisted, i don't process it")
+               nowhite = ("This is your ChatID: %d" % chat_id)
+               bot.send_message(chat_id=chat_id, text=nowhite)
 ################################################################################
+
         # Get Bot Token from settings.json
         bottoken = self.settings.get('TeleToken', '')
 
         # Connection to Telegram API
         self.telegram_updater = Updater(token=bottoken) # get telegram Updates
         telegram_dispatcher = self.telegram_updater.dispatcher
-        logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+       # logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.WARNING)
         start_handler = MessageHandler(Filters.text, telegramMessages)
         telegram_dispatcher.add_handler(start_handler)
         self.telegram_updater.start_polling(clean=True) # start clean and look for messages
 
-    def shutdown(self):
-        self.telegram_updater.stop()
+    def shutdown(self): # shutdown routine
+        self.telegram_updater.stop() # stop updater
         self.telegram_updater.is_idle = False
         super(TelegramSkill, self).shutdown()
 
