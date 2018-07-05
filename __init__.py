a79e2d6f905ba31f0b187c922340a2e5bc94f3ef
Luke5Sky <luke.gangel87@gmail.com>
2018-07-05 18:23:12 +0200

    Eventhandler Added + multiple device support

--- __init__.py
+++ __init__.py
@@ -19,7 +19,7 @@ import re
 import logging
 import sys
 import time
-
+from alsaaudio import Mixer
 
 from adapt.intent import IntentBuilder
 from mycroft.skills.core import MycroftSkill, intent_handler
@@ -27,7 +27,13 @@ from mycroft.util.log import LOG
 from telegram.ext import Updater, MessageHandler, Filters, CommandHandler
 from websocket import create_connection, WebSocket
 from mycroft.messagebus.message import Message
+from mycroft.api import DeviceApi
+from mycroft.audio import wait_while_speaking
+from mycroft.util.log import getLogger
 
+logger = getLogger(__name__)
+global_resp = ""
+speak_tele = 0
 
 __author__ = 'luke5sky'
 
@@ -36,56 +42,88 @@ class TelegramSkill(MycroftSkill):
     def __init__(self):
         super(TelegramSkill, self).__init__(name="TelegramSkill")
 
+    def initialize(self):
+        self.mute = self.settings.get('MuteIt','')
+        self.mixer = Mixer()
+        self.add_event('recognizer_loop:audio_output_start', self.muteHandler)
         user_id1 = self.settings.get('TeleID1', '')
         user_id2 = self.settings.get('TeleID2', '')
         user_id3 = self.settings.get('TeleID3', '')
         user_id4 = self.settings.get('TeleID4', '')
         chat_whitelist = [user_id1,user_id2,user_id3,user_id4]
-#	    get_intro_message()
-################################################################################
-        def telegramMessages(bot, update):
+        # Get Bot Token from settings.json
+        UnitName = DeviceApi().get()['name']
+        MyCroftDevice1 = self.settings.get('MDevice1','')
+        MyCroftDevice2 = self.settings.get('MDevice2','')
+        bottoken = ""
+        if MyCroftDevice1 == UnitName:
+           logger.debug("found dev1 " + UnitName)
+           bottoken = self.settings.get('TeleToken1', '')
+        elif MyCroftDevice2 == UnitName:
+           logger.debug("Found Dev2 " + UnitName)
+           bottoken = self.settings.get('TeleToken2', '')
+        else:
+           logger.info("No or incorrect Device Name specified! Your DeviceName is: " + UnitName + " " + MyCroftDevice1 + " " + MyCroftDevice2)
+
+        def getTelegramMessages(bot, update):
             msg = update.message.text
             chat_id = str(update.message.chat_id)
-            
             if chat_id in chat_whitelist:
-               print("Telegram-Message from User: " + msg)
-               uri = 'ws://localhost:8181/core'
-               ws = create_connection(uri)
-               utt = '{"context": null, "type": "recognizer_loop:utterance", "data": {"lang": "en-us", "utterances": ["' + msg + '"]}}'
-               ws.send(utt)
- #              def handle_listener_started(self):
- #                  print("listener started wupdidudl: ")
- #              self.add_event('recognizer_loop:utterance', handle_listener_started)
-               mtime = time.time()
-               count = 0
-               while count < 10:
-                     test_resp = ws.recv()
-                     if ('"utterance"' in test_resp) and ('"speak"' in test_resp):
-                #        print("Found response: " + test_resp)
-                        response = re.sub(".*utterance\":\s\"([\w\s'(),.;:?!]+)\".*", r"\1", test_resp)
-                        print("Response to Telegram Message found: " + response)
-                        bot.send_message(chat_id=chat_id, text=response)
-                        mtime = time.time()
-                        break
-                     now = time.time()
-                     count = now - mtime
-               ws.close()
+               logger.info("Telegram-Message from User: " + msg)
+               global speak_tele
+             #  speak_tele = 1
+               self.add_event('speak', responseHandler)
+               sendMycroftUtt(msg)
+               global global_resp
+               while len(global_resp) < 1:
+                     pass
+            
+               if len(global_resp) > 1:
+                  bot.send_message(chat_id=chat_id, text=global_resp)
+                  global_resp = ""
+                  print(speak_tele)
+               speak_tele = 0
             else:
-               print("Chat ID " + user_id1 + " is not whitelisted, i don't process it")
+               logger.info("Chat ID " + user_id1 + " is not whitelisted, i don't process it")
                nowhite = ("This is your ChatID: " + chat_id)
                bot.send_message(chat_id=chat_id, text=nowhite)
-################################################################################
 
-        # Get Bot Token from settings.json
-        bottoken = self.settings.get('TeleToken', '')
+        def sendMycroftUtt(msg):
+            uri = 'ws://localhost:8181/core'
+            ws = create_connection(uri)
+            utt = '{"context": null, "type": "recognizer_loop:utterance", "data": {"lang": "en-us", "utterances": ["' + msg + '"]}}'
+            global speak_tele
+            speak_tele = 1
+            #self.add_event('speak', responseHandler)
+            ws.send(utt)
+            ws.close()
 
+        def responseHandler(message):
+            global speak_tele
+            print(speak_tele)
+            if speak_tele == 1:
+               telresp = message.data.get("utterance")
+               logger.info("Response for Telegram-User: " + telresp )
+               global global_resp
+               global_resp = telresp
+            speak_tele = 0
+            
         # Connection to Telegram API
         self.telegram_updater = Updater(token=bottoken) # get telegram Updates
-        telegram_dispatcher = self.telegram_updater.dispatcher
-       # logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.WARNING)
-        start_handler = MessageHandler(Filters.text, telegramMessages)
-        telegram_dispatcher.add_handler(start_handler)
+        self.telegram_dispatcher = self.telegram_updater.dispatcher
+        receive_handler = MessageHandler(Filters.text, getTelegramMessages)
+        self.telegram_dispatcher.add_handler(receive_handler)
         self.telegram_updater.start_polling(clean=True) # start clean and look for messages
+
+    def muteHandler(self, message):
+        global speak_tele
+        print("var ##" + self.mute + "##### %d " % speak_tele )
+        if self.mute == 'true' and speak_tele == 1:
+           volume_level = self.mixer.getvolume()[0]
+           self.mixer.setvolume(0)
+           wait_while_speaking()
+           self.mixer.setvolume(volume_level)
+           speak_tele = 0
 
     def shutdown(self): # shutdown routine
         self.telegram_updater.stop() # stop updater
@@ -93,6 +131,7 @@ class TelegramSkill(MycroftSkill):
         super(TelegramSkill, self).shutdown()
 
     def stop(self):
+ #       self.speak_tele = 0
         pass
 
 def create_skill():
